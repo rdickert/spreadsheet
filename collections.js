d0c3f26ecb69e4d00fbd170f065663501ebdb793
Robert Dickert <robert.dickert@gmail.com>
2016-01-24 18:37:05 -0700

    delete unused code

--- collections.js
+++ collections.js
@@ -1,11 +1,2 @@
 Cells = new Mongo.Collection("cells");
 
-// if (Meteor.isServer) {
-//   if (!Cells.findOne()) {
-//     for (let row = 0; row < 21; row++) {
-//       for (let col = 0; col < 11; col++) {
-//         Cells.insert({row, col, text: ""});
-//       }
-//     }
-//   }
-// }
--- spreadsheet.jsx
+++ spreadsheet.jsx
@@ -1,7 +1,5 @@
 if (Meteor.isClient) {
   Meteor.startup(function() {
-    // Use Meteor.startup to render the component after the page is ready
-
     // Wait on subscription ready or there is a race condition.
     Meteor.subscribe("sheet", {
       onReady () {
